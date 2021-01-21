9566d41e791d207e55f587d5f01985379d70a712
Eric Ober <ericscottober@gmail.com>
2021-01-20 19:09:01 -0500

    Created seeds sql file

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Gordos Burger');
+INSERT INTO burgers (burger_name) VALUES ('Jean-Robert Burger');
+INSERT INTO burgers (burger_name) VALUES ('French Burger');
