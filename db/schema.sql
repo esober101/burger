90609e968326fc9f90e7770722d03afcfef4d4fc
Eric Ober <ericscottober@gmail.com>
2021-01-26 12:58:36 -0500

    Created schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,12 @@
-CREATE DATABASE burgers_db;
+### Schema
 
-USE burgers_db;
+CREATE DATABASE burger_db;
+USE burger_db;
 
 CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
-	burger_name varchar(255) NOT NULL,
+	name varchar(255) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
